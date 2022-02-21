ef657e5802217fbc516024b1298df92129d60c33
krister <looga.krister@gmail.com>
2022-02-21 10:48:39 +0200

    Only submit answers with definitive value

--- src/components/checksView/ChecksView.tsx
+++ src/components/checksView/ChecksView.tsx
@@ -28,7 +28,8 @@ export const ChecksView = ({ handleSubmit }: ChecksViewProps) => {
       return;
     }
 
-    const data: SubmitData = Object.values(checks).map((check) => {
+    const checksWithDefinitiveValue = Object.values(checks).filter(check => check.value !== undefined);
+    const data: SubmitData = checksWithDefinitiveValue.map((check) => {
       return {
         checkId: check.id,
         result: check.value,
